07b929a1a912c5cf0a7260bed2e545588c06b65b
himanshi-2005 <himanshihimanshi631@gmail.com>
2024-09-06 10:29:03 +0530

    onclick,nested layout

--- app/src/main/java/com/example/linearlayouttask/Linear_layout_horizontal.kt
+++ app/src/main/java/com/example/linearlayouttask/Linear_layout_horizontal.kt
@@ -10,55 +10,37 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-
-    var a :EditText?=null
-   var b:Button?=null
+class Linear_layout_horizontal : AppCompatActivity() {
     var c:EditText?=null
     var d:Button?=null
     var e:EditText?=null
     var f:Button?=null
-    var g:EditText?=null
-   var h:Button?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_linear_layout_horizontal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+c=findViewById(R.id.edt2)
+d=findViewById(R.id.btn2)
+d?.setOnClickListener {
+    if(c?.text.toString().isNullOrBlank()){
+        c?.error="value required"
+        Toast.makeText(this,"Enter a value",Toast.LENGTH_SHORT
+        ).show()
+    }
+    else{
+        var intent = Intent(this,Textview::class.java)
+        startActivity(intent)
+    }
 
-        }
-        a=findViewById(R.id.edt1)
-        b=findViewById(R.id.btn1)
-        b?.setOnClickListener{
-            if(a?.text.toString().isNullOrBlank()){
-                a?.error="value required"
-                Toast.makeText(this,"Enter a value",Toast.LENGTH_SHORT
-                    ).show()
-            }
-            else{
-                var intent = Intent(this,Linear_layout_horizontal::class.java)
-                startActivity(intent)
-            }
-        }
+}
 
-        c=findViewById(R.id.edt2)
-        d=findViewById(R.id.btn2)
-        d?.setOnClickListener{
-            if(c?.text.toString().isNullOrBlank()){
-                c?.error="value required"
-                Toast.makeText(this,"Enter a value",Toast.LENGTH_SHORT
-                ).show()
-            }
-            else{
-                var intent = Intent(this,Textview::class.java)
-                startActivity(intent)
-            }
-        }
         e=findViewById(R.id.edt3)
         f=findViewById(R.id.btn3)
         f?.setOnClickListener{
@@ -72,7 +54,6 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
 
     }
 }
